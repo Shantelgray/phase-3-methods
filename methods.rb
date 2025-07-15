c11e546a1355ac3e93d330fbda332a7fd849905e
Shantel <Shantelgray24@gmail.com>
2025-07-14 22:17:20 -0400

    Methods Lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
-# Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "Naureen")
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add num1, num2
+  puts num1
+  puts num2
+  num1 + num2
+end
+
+puts add(1,3) + add(2,3)
+
+def halve(number)
+  if number.class !=Integer
+    return nil
+  end
+  number / 2
+end
+  
